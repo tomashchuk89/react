fddb357be709da4902dbd29cd3e10a5c9fbaec28
tomashchuk89 <oleksandr.tomashchuk89@gmail.com>
2023-12-10 15:15:47 +0200

    hw1

---
+++ hw_1/src/components/card/card.jsx
@@ -0,0 +1,23 @@
+
+import './card.css';
+
+
+
+const Card = () => {
+
+    return (
+        <>
+            <div className='card'>
+                <h1>Card Title</h1>
+                <p>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </p>
+                <button className='btn'> Read more...</button> 
+        </div>       
+       </>
+                        
+    )
+}
+
+export default Card
