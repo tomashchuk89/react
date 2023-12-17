9a1ff536187af1b3df4d135c2bfef9471ac18e6f
tomashchuk89 <oleksandr.tomashchuk89@gmail.com>
2023-12-17 16:58:51 +0200

    hw2

---
+++ hw_2/src/App.js
@@ -0,0 +1,52 @@
+
+import './App.css';
+import Card from './components/card/card';
+
+function App() {
+  const cources = [
+    {
+      id: 1,
+      image:
+        "https://ultimatecourses.com/assets/share/courses/react-f02200115da09fd485a296e351972d7ea75701ed8d3d023d9f18c4b38e6b18b0.png",
+      level: "Intermediate",
+      title: "Introduction Basic Programming HTML & CSS",
+      user: {
+        avatar:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
+        name: "John Doe"
+      },
+      rating: 4.5,
+      students: 1234,
+      modules: 5,
+      duration: 5400
+    },
+    {
+      id: 2,
+      image:
+        "https://ultimatecourses.com/assets/share/courses/react-f02200115da09fd485a296e351972d7ea75701ed8d3d023d9f18c4b38e6b18b0.png",
+      level: "Intermediate",
+      title: "Introduction Basic Programming HTML & CSS",
+      user: {
+        avatar:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
+        name: "John Doe"
+      },
+      rating: 4.5,
+      students: 1234,
+      modules: 5,
+      finishedModules: 3,
+      duration: 5500,
+      isMyCource: true
+    }
+  ];
+  
+  return (
+    <div className="App">
+
+      <Card cources={cources}/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ hw_2/src/components/card/card.jsx
@@ -0,0 +1,62 @@
+import './card.css';
+
+
+const Card = ({cources}) => {
+   
+    const hours = (duration) => {
+        const milliseconds = duration  * 1000; 
+        const date = new Date(milliseconds);
+      
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+        const hoursMinutes = `${hours} h ${minutes} m`;
+      
+        return hoursMinutes;
+      }
+
+
+    return (
+        <>
+        <ul className="cards">
+            {cources.map(({id, image, level, title, user, rating, students, modules, duration})=>
+                <li key={id}>
+                    <div className="card">
+                            <div className="image__container">
+                                <img
+                                src={image}
+                                alt=""
+                                />
+                                <div className="level">{level}</div>
+                            </div>
+                            <h3 className="card__title">{title}</h3>
+
+                            <div className="card__info">
+                                <div className="user">
+                                <img
+                                    src={user.avatar}
+                                    alt={user.name}
+                                />
+                                <p>{user.name}</p>
+                                </div>
+                                <div className="rating">
+                                <p>{rating}</p>
+                                </div>
+                            </div>
+
+                            
+
+                            <div className="card__info">
+                                <div>{students} Student</div>
+                                <div>{modules} Modules</div>
+                                <div>{hours(duration)}</div>
+                            </div>
+                    </div> 
+                </li>
+            )}
+         </ul>
+       </>
+                        
+    )
+}
+
+export default Card
